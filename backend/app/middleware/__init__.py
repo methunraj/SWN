0dff76b23fbb5b589e077f5d8836cf26286ac62f
methunraj <102622563+methunraj@users.noreply.github.com>
2025-06-16 21:10:13 +0200

    up

--- backend/app/middleware/__init__.py
+++
@@ -1,5 +0,0 @@
-from .cors import setup_cors
-from .rate_limit import setup_rate_limit
-from .error_handler import setup_exception_handlers
-
-__all__ = ["setup_cors", "setup_rate_limit", "setup_exception_handlers"]
